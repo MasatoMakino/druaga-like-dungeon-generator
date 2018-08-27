f7f2a802a601f3f5c1924977dfe30ca204e320de
MasatoMakino <unagiinu.g@gmail.com>
2018-08-27 14:53:11 +0900

    ヘッダーカラーの調整。コントロールをフローティングフッターに集約。

--- src/js/main.js
+++ src/js/main.js
@@ -4,18 +4,42 @@ var generator;
 
 window.onload = function() {
   generator = new DungeonGenerator();
+  updateMapCSS();
   updateMap();
 
   document.getElementById("seed").addEventListener("input", () => {
+    updateMap();
+  });
+
+  document.getElementById("floorW").addEventListener("input", () => {
+    updateMapCSS();
+    updateMap();
+  });
+
+  document.getElementById("floorH").addEventListener("input", () => {
     updateMap();
   });
 };
 
 const updateMap = () => {
   const startTime = performance.now(); // 開始時間
+
   let seed = parseInt(document.getElementById("seed").value);
-  document.getElementById("map").innerHTML = generator.generate(seed);
+  let floorW = parseInt(document.getElementById("floorW").value);
+  let floorH = parseInt(document.getElementById("floorH").value);
+  document.getElementById("map").innerHTML = generator.generate(
+    seed,
+    floorW,
+    floorH
+  );
 
   const endTime = performance.now(); // 終了時間
   console.log(endTime - startTime); // 何ミリ秒かかったかを表示する
+};
+
+const updateMapCSS = () => {
+  let floorW = parseInt(document.getElementById("floorW").value);
+  const map = document.getElementById("mapContainer");
+  const fontSize = map.clientWidth / (floorW * 2.37);
+  map.style.fontSize = fontSize + "px";
 };
