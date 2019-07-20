abdd62df68d305d9caddea24928891b2c9243cb6
MasatoMakino <unagiinu.g@gmail.com>
2019-07-20 12:50:50 +0900

    依存パッケージ更新

--- webpack.config.script.js
+++ webpack.config.script.js
@@ -1,5 +1,4 @@
 "use strict";
-const path = require("path");
 const webpack = require("webpack");
 
 module.exports = (env, argv) => {
@@ -18,14 +17,9 @@ module.exports = (env, argv) => {
       rules: [
         {
           test: /\.js$/,
-          loader: "babel-loader",
-          options: {
-            presets: [
-              // env を指定することで、ES2018 を ES5 に変換。
-              // {modules: false}にしないと import 文が Babel によって CommonJS に変換され、
-              // webpack の Tree Shaking 機能が使えない
-              ["env", { modules: false }]
-            ]
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
           }
         }
       ]
